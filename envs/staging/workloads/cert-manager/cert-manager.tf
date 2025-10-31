5dbe2c3c30f4eb7973a6fed5862a572784dce50f
Ochuko Whoro <ckcodemax@gmail.com>
2025-10-31 23:13:25 +0100

    updated cert manager

--- envs/staging/workloads/cert-manager/cert-manager.tf
+++ envs/staging/workloads/cert-manager/cert-manager.tf
@@ -1,46 +1,16 @@
-##############################################################
-# 1️⃣ Namespace for cert-manager
-##############################################################
-resource "kubernetes_namespace" "cert_manager" {
-  metadata {
-    name = "cert-manager"
-  }
-
-  depends_on = [
-    data.aws_eks_cluster.eks,
-    data.aws_eks_cluster_auth.eks
-  ]
-}
-
-##############################################################
-# 2️⃣ Helm release for cert-manager
-##############################################################
 resource "helm_release" "cert_manager" {
-  name             = "cert-manager"
+  name = "cert-manager"
+
   repository       = "https://charts.jetstack.io"
   chart            = "cert-manager"
-  namespace        = kubernetes_namespace.cert_manager.metadata[0].name
+  namespace        = "cert-manager"
   create_namespace = true
   version          = "v1.14.5"
-  atomic           = true
-  timeout          = 900
-
-  # ✅ Reference an external Helm values file
-  values = [file("${path.module}/values/cert-manager-values.yaml")]
-
-  # Explicitly disable Prometheus ServiceMonitor to avoid CRD dependency
-  set {
-    name  = "prometheus.enabled"
-    value = "false"
-  }
 
   set {
-    name  = "prometheus.servicemonitor.enabled"
-    value = "false"
+    name  = "installCRDs"
+    value = "true"
   }
 
-  depends_on = [
-    data.terraform_remote_state.eks,
-    kubernetes_namespace.cert_manager
-  ]
+  depends_on = [helm_release.external_nginx]
 }
